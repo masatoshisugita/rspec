3a23f61f1ec3319c873412bfac4724d89087fe10
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-03-22 14:59:03 +0900

    10 chapter geocode

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'faker', require: false # for sample data in development
+  gem 'rb-readline'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "SignIn", type: :feature do
+  let(:user){ FactoryBot.create(:user)}
+
+  before do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
+  #ユーザーのログイン
+  scenario "user sign in" do
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email" ,with: user.email
+    fill_in "Password" ,with: user.password
+    click_button "Log in"
+
+    expect{ GeocodeUserJob.perform_later(user)}.to have_enqueued_job.with(user)
+  end
+end
